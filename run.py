b914698b8540a34e545cc5cb4567ca6aad0b03a7
Thirukumaran25 <kumaran2597@gmail.com>
2025-08-20 13:08:26 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-from app.models import User, Product
+from app.models import Product
 
 app = create_app()
 
@@ -9,15 +9,16 @@ if __name__ == '__main__':
 
         if not Product.query.first():
             print("Adding initial products...")
-            p1 = Product(name="Laptop", price=50000.00, description="A powerful laptop for work and gaming.")
-            p2 = Product(name="Wireless Mouse", price=450.50, description="Ergonomic mouse with a long battery life.")
-            p3 = Product(name="Keyboard", price=750.00, description="Mechanical keyboard with custom RGB lighting.")
-            p4 = Product(name="Moniter", price=2750.00, description="4k 15.6inch moniter with Wifi options.")
-            p5 = Product(name="Webcam", price=60.00, description="Full HD webcam with a built-in microphone.")
+            products = [
+                Product(name="Laptop", price=50000.00, description="A powerful laptop for work and gaming."),
+                Product(name="Wireless Mouse", price=450.50, description="Ergonomic mouse with a long battery life."),
+                Product(name="Keyboard", price=750.00, description="Mechanical keyboard with custom RGB lighting."),
+                Product(name="Monitor", price=2750.00, description="4K 15.6-inch monitor with WiFi options."),
+                Product(name="Webcam", price=60.00, description="Full HD webcam with a built-in microphone."),
+            ]
 
-
-            db.session.add(p1)
+            db.session.add_all(products)
             db.session.commit()
             print("Initial products added!")
-            
+
     app.run(debug=True)
